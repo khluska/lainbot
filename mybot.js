3650aca133888893d51d8acfc9635c4e648727d5
Nick <nick.hluska@gmail.com>
2017-01-25 19:58:59 -0500

    added config file, v0.0.3

--- mybot.js
+++ mybot.js
@@ -1,8 +1,7 @@
 var Discord = require("discord.js");
-//var parseString = require('xml2js').parseString;
+var config = require('./config.json');
 var client = new Discord.Client();
-var prefix = "!";
-var akkoArray = ["http://pa1.narvii.com/6140/983b2c7fd40022458024a007b1359fb6dcd9d4b4_hq.gif", "http://vignette2.wikia.nocookie.net/young-animator-training-project/images/5/50/Akko.jpg/revision/latest?cb=20130421150105", "http://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/akko-kagari-little-witch-academia-1.75.jpg", "https://www.anime-planet.com/images/characters/atsuko-kagari-50037.jpg", "http://static1.fjcdn.com/thumbnails/comments/Don+t+lie+babe+we+all+know+you+re+a+filthy+slut+_6e746044df3cf6e7c9f9087ce4582f53.jpg"];
+
          
 
 
@@ -13,21 +12,35 @@ function getRandomImage(a) {
 }
 
 client.on("message", msg => {
-    if (msg.content.startsWith(prefix + "jaraxxus") || msg.content.startsWith(prefix + "hearthstone")) {
+    if (msg.content.startsWith(config.prefix + "jaraxxus") || msg.content.startsWith(config.prefix + "hearthstone")) {
         msg.channel.sendMessage("https://www.youtube.com/watch?v=MkeC9WVVsQ4");
     }
-    if (msg.content.startsWith(prefix + "sonic")) {
+    if (msg.content.startsWith(config.prefix + "sonic")) {
         msg.channel.sendMessage("Gotta go fast");
     }
-    if (msg.content.startsWith(prefix + "akko")) {
-        msg.channel.sendMessage(getRandomImage(akkoArray));
-        
+    if (msg.content.startsWith(config.prefix + "f")) {
+        msg.channel.sendMessage("Paying Respects");
+    }
+    if (msg.content.startsWith(config.prefix + "cuck")) {
+        msg.channel.sendMessage("@Alex ");
+    }
+    if (msg.content.startsWith(config.prefix + "shit")) {
+        msg.channel.sendMessage("Your Waifu");
+    }
+    if (msg.content.startsWith(config.prefix + "bots")) {
+        msg.channel.sendMessage("are cool");
+    }
+    //if (msg.content.startsWith(config.prefix + "angery")) {
+    //    PUT /channels/{channel.id}/messages/{message.id}/reactions/270029665798717461/@me;
+    //}
+    if (msg.content.startsWith(config.prefix + "akko")) {
+        msg.channel.sendMessage(getRandomImage(config.imgArrayA));
     }
     
 });
 
 
 client.on('ready', () => {
-    console.log('Ready! v0.0.2');
+    console.log('Ready! v0.0.3');
 });
-client.login("MjcyNTgxMDQzNzAwMjM2Mjg5.C2XIFQ.P238B_wQA2aCX3D3VtaD8mCHdAw");
+client.login(config.token);
