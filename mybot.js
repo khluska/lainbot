8474db4e2931d7bb39055e671d0c21ed5b98ca84
Nick <nick.hluska@gmail.com>
2018-02-23 15:12:27 -0500

    smol update to remove old broken functionality

--- mybot.js
+++ mybot.js
@@ -12,27 +12,16 @@ function getRandomImage(a) {
 }
 
 client.on("message", msg => {
-    if (msg.content.startsWith(config.prefix + "jaraxxus") || msg.content.startsWith(config.prefix + "hearthstone")) {
-        msg.channel.sendMessage("https://www.youtube.com/watch?v=MkeC9WVVsQ4");
-    }
+    
     if (msg.content.startsWith(config.prefix + "sonic")) {
         msg.channel.sendMessage("Gotta go fast");
     }
     if (msg.content.startsWith(config.prefix + "f")) {
         msg.channel.sendMessage("Paying Respects");
     }
-    if (msg.content.startsWith(config.prefix + "cuck")) {
-        msg.channel.sendMessage("@Alex ");
-    }
     if (msg.content.startsWith(config.prefix + "shit")) {
         msg.channel.sendMessage("Your Waifu");
     }
-    if (msg.content.startsWith(config.prefix + "bots")) {
-        msg.channel.sendMessage("are cool");
-    }
-    //if (msg.content.startsWith(config.prefix + "angery")) {
-    //    PUT /channels/{channel.id}/messages/{message.id}/reactions/270029665798717461/@me;
-    //}
     if (msg.content.startsWith(config.prefix + "akko")) {
         msg.channel.sendMessage(getRandomImage(config.imgArrayA));
     }
